76c0931f5bd1d02e656b41e66873aba8cf6b875c
kw-shakshi3104 <103113049+kw-shakshi3104@users.noreply.github.com>
2022-09-22 09:17:33 +0900

    Initial Commit

---
+++ Dashboard/DashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  DashboardApp.swift
+//  Dashboard
+//
+//  Created by 小林慧 on 2022/09/22.
+//
+
+import SwiftUI
+
+@main
+struct DashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
